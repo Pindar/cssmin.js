04ea987bcf6b20bf4b4f6e7c42f6063675549d2b
Simon Dittlmann <simon@itnotes.de>
2012-06-15 22:39:52 +0200

    fixed issues with different import styles and modified tests to cover this problems.

--- src/combine.js
+++ src/combine.js
@@ -3,12 +3,14 @@ var SD = SD || {};
 
 SD.combineCss = function (css) {
     "use strict";
-    var regex = /@import?\s+\S*/g;
+    var regex = /@import?\s+[\S\s]*;$/g;
 
     function replaceImportIn(css1) {
         return css1.replace(regex, function (str) {
-            var files = str.split('"')[1];
-            return readFile(files);
+            var files;
+            str = str.replace(/'/g, '"');
+            files = str.split('"')[1];
+            return (files) ? readFile(files) : "";
         });
     }
 
