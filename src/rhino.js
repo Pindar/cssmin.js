b8614dcf43b56b51931f0d3e8114a30db2f724ae
Simon Dittlmann <simon@itnotes.de>
2012-06-15 22:45:24 +0200

    fixed jslint and jshint issues.

--- src/rhino.js
+++ src/rhino.js
@@ -1,24 +1,31 @@
+/*global SD, YAHOO, java, print, quit, readFile*/
 function writeFile(filename, content) {
-    var fstream = new java.io.FileWriter(filename);
-    var out = new java.io.BufferedWriter(fstream);
+    "use strict";
+    var fstream = new java.io.FileWriter(filename),
+        out = new java.io.BufferedWriter(fstream);
     out.write(content);
     out.close();
 }
 
 // Command line integration via Rhino
 (function (args) {
+    "use strict";
     var name = args[0],
         outputFile = args[1],
-        linebreakpos= args[2],
+        linebreakpos = args[2],
         output,
         input,
-        combinededInput = "";
+        combinededInput = "",
+        path = "";
 
     if (!name) {
         print('No files present in the fileset; Check your pattern match in build.xml');
         quit(1);
     }
-    path = name.split("/");path.pop();path=path.join("/");
+
+    path = name.split("/");
+    path.pop();
+    path = path.join("/");
 
     input = readFile(name);
 
@@ -26,14 +33,14 @@ function writeFile(filename, content) {
         print('cssmin: couldn\'t open file ' + name);
         quit(1);
     }
-    
+
     if (!outputFile) {
         print('cssmin: couldn\'t write output file ' + outputFile);
         quit(1);
     }
-    
+
     combinededInput = SD.combineCss(input);
-    
+
     output = YAHOO.compressor.cssmin(combinededInput, linebreakpos);
     writeFile(outputFile, output);
     print('done');
